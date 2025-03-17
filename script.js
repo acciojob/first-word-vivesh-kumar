045ae3fd074d743e237497036ca68f03d0d9a6e7
Vivesh kumar <vivesh103@gmail.com>
2025-03-17 18:58:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function firstWord(s) {
   // your code here
+	let patt =([^\s]+);
+	return patt.test(s);
 }
 
 // Do not change the code below
 
+
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
